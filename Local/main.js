a9c7abfff59629ef697edd486b47a2823cb382c3
Daksh <daksh3451@gmail.com>
2020-08-22 09:20:02 -0400

    changed tray icon

--- Local/main.js
+++ Local/main.js
@@ -1,4 +1,7 @@
-const { app, BrowserWindow, Menu, Tray } = require('electron')
+const path = require("path")
+
+const { app, BrowserWindow, Menu, Tray, nativeImage } = require('electron')
+const psList = require('ps-list');
 
 function createWindow() {
 	const win = new BrowserWindow({
@@ -11,8 +14,8 @@ function createWindow() {
 
 	win.loadFile('index.html');
 
-	let tray = new Tray('C:/Users/aryan/OneDrive/Pictures/rbc.png');
-	tray.setToolTip('[Insert Name Here]');
+	let tray = new Tray(nativeImage.createFromPath(path.join(__dirname, "obi wan.png")));
+	tray.setToolTip("It's Over Anakin");
 	const contextMenu = Menu.buildFromTemplate([
 		{ label: 'Item1', type: 'radio' },
 		{ label: 'Item2', type: 'radio', checked: true }
@@ -22,3 +25,4 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
+
