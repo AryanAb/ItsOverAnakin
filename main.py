dc92adb176faeec122f37fa672f303e938860808
Aryan <aryanabed48@gmail.com>
2020-08-21 22:55:17 -0400

    Added Backend for Logg In and Sign Up

--- main.py
+++ main.py
@@ -9,23 +9,39 @@ app.secret_key = os.urandom(24)
 app.config['SECRET_KEY'] = "afAYG8o5y4waP;JNGSOI205O3"
 
 firebase = pyrebase.initialize_app(json.load(open("app/firebase.json")))
+auth = firebase.auth()
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    if "user" not in session:
+        return render_template("index.html")
+    return redirect(url_for("home"))
 
 
 @app.route("/login", methods=["POST"])
 def log_in():
-    username = request.form["user_up"]
+    email = request.form["email_up"]
     password = request.form["pass_up"]
+    user = auth.sign_in_with_email_and_password(email, password)
+    session["user"] = user
+    return redirect(url_for("home"))
 
 
 @app.route("/signup", methods=["POST"])
 def sign_up():
-    pass
+    email = request.form["email_in"]
+    password = request.form["pass_in"]
+    auth.create_user_with_email_and_password()
+    user = auth.sign_in_with_email_and_password(email, password)
+    session["user"] = user
+    return redirect(url_for("home"))
+
+
+@app.route("/home")
+def home():
+    render_template("home.html")
 
 
 if __name__ == "__main__":
